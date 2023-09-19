d770cc29007eafcdec4f6e04dfeb139bd4c92b2c
archer7717 <128358378+archer7717@users.noreply.github.com>
2023-09-19 11:45:19 +0300

    aboba431.sh

--- aboba431.sh
+++ aboba431.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd ~
-mkdir Leocad && cd Leocad
+mkdir Leocad1 && cd Leocad1
 wget https://github.com/leozide/leocad/releases/download/v23.03/LeoCAD-Linux-23.03-x86_64.AppImage
 chmod a+x  LeoCAD-Linux-23.03-x86_64.AppImage
 cd ~
-mkdir Freecad && cd  Freecad
+mkdir Freecad1 && cd  Freecad1
 wget https://github.com/FreeCAD/FreeCAD/releases/download/0.21.1/FreeCAD_0.21.1-Linux-x86_64.AppImage
 chmod a+x  FreeCAD_0.21.1-Linux-x86_64.AppImage
 cd ~
-mkdir Pascal && cd Pascal
+mkdir Pascal1 && cd Pascal1
 wget https://pascalabc.net/downloads/PascalABCNETLinux.zip
 unzip PascalABCNETLinux.zip
 
@@ -23,8 +23,8 @@ sudo apt install wine -y
 cd ~
 cd /opt
 git clone https://git.niisi.ru/kumir/kumir2.git
-cd kumir2
-mkdir build && cd build
+cd kumir3
+mkdir build1 && cd build1
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DUSE_QT=5 ..
 make
 sudo make install
