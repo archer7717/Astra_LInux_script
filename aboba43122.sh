91750998e4309cd5d736ef81ded76459c2d62c5b
archer7717 <128358378+archer7717@users.noreply.github.com>
2023-09-19 23:54:18 +0300

    aboba43122.sh

--- aboba43122.sh
+++ aboba43122.sh
@@ -5,7 +5,7 @@ if [ -d  Leocad ]
 then
 echo "Католог Leocad  уже существует"
 else
-mkdir Leocad && cd Leocad;wget https://github.com/leozide/leocad/releases/download/v23.03/LeoCAD-Linux-23.03-x86>
+mkdir Leocad && cd Leocad;wget https://github.com/leozide/leocad/releases/download/v23.03/LeoCAD-Linux-23.03-x86_64.AppImage;chmod a+x LeoCAD-Linux-23.03-x86_64.AppImage
 
 fi
 
@@ -16,7 +16,7 @@ then
 echo "Католог Freecad  уже существует"
 else
 
-mkdir Freecad && cd  Freecad;wget https://github.com/FreeCAD/FreeCAD/releases/download/0.21.1/FreeCAD_0.21.1-Lin>
+mkdir Freecad && cd  Freecad;wget https://github.com/FreeCAD/FreeCAD/releases/download/0.21.1/FreeCAD_0.21.1-Linux-x86_64.AppImage;chmod a+x  FreeCAD_0.21.1-Linux-x86_64.AppImage
 
 fi
 
@@ -35,7 +35,7 @@ sudo apt-get install gcc g++ -y
 sudo apt-get install cmake -y
 sudo apt-get install python3 -y
 sudo apt-get install zlibc zlib1g zlib1g-dev -y
-sudo apt-get install qt5-default qttools5-dev-tools libqt5webkit5-dev libqt5multimediawidgets5 libqt5network5 li>
+sudo apt-get install qt5-default qttools5-dev-tools libqt5webkit5-dev libqt5multimediawidgets5 libqt5network5 libqt5svg5-dev libqt5x11extras5-dev qtscript5-dev -y
 sudo apt-get install git-core -y
 sudo apt install wine -y
 
@@ -46,3 +46,4 @@ echo "Католог kumir2  уже существует"
 else
 mkdir kumir2 && cd kumir2;cmake -DCMAKE_INSTALL_PREFIX=/usr -DUSE_QT=5 ..;sudo make;sudo make install
 fi
+
