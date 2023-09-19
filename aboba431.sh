e3ed8c80cfdd8d3659f190a392cec3e40442db2c
archer7717 <128358378+archer7717@users.noreply.github.com>
2023-09-19 14:36:16 +0300

    aboba431.sh

--- aboba431.sh
+++ aboba431.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cd ~
+cd /home/astra
 mkdir Leocad1 && cd Leocad1
 wget https://github.com/leozide/leocad/releases/download/v23.03/LeoCAD-Linux-23.03-x86_64.AppImage
 chmod a+x  LeoCAD-Linux-23.03-x86_64.AppImage
-cd ~
+cd /home/astra
 mkdir Freecad1 && cd  Freecad1
 wget https://github.com/FreeCAD/FreeCAD/releases/download/0.21.1/FreeCAD_0.21.1-Linux-x86_64.AppImage
 chmod a+x  FreeCAD_0.21.1-Linux-x86_64.AppImage
-cd ~
+cd /home/astra
 mkdir Pascal1 && cd Pascal1
 wget https://pascalabc.net/downloads/PascalABCNETLinux.zip
 unzip PascalABCNETLinux.zip
@@ -20,7 +20,7 @@ sudo apt-get install zlibc zlib1g zlib1g-dev -y
 sudo apt-get install qt5-default qttools5-dev-tools libqt5webkit5-dev libqt5multimediawidgets5 libqt5network5 libqt5svg5-dev libqt5x11extras5-dev qtscript5-dev -y
 sudo apt-get install git-core -y
 sudo apt install wine -y
-cd ~
+cd /home/astra
 cd /opt
 git clone https://git.niisi.ru/kumir/kumir2.git
 cd kumir3
